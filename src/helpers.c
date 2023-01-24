1622dfd9bba1378fdeaed7cf82f3e053f2035234
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2023-01-24 23:20:14 +0100

    first commit

---
+++ src/helpers.c
@@ -0,0 +1,6 @@
+#include "helpers.h"
+
+int Sum(int a, int b)
+{
+    return a+b;
+}
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "helpers.h"
+
+int main()
+{
+    int result = Sum(1, 2);
+    printf("%3d\t", result);
+    printf("Hello World");
+}
